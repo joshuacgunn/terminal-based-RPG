b4dea3a0fe4755f9666c99e5c18a4004fb325cc4
joshuacgunn <181609261+joshuacgunn@users.noreply.github.com>
2025-03-11 16:03:38 -0400

    Added functionality for LIGHTNING and ARCANE elements

--- MageClass.java
+++ MageClass.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MageClass extends Player {
@@ -17,8 +18,18 @@ public class MageClass extends Player {
                 target.addStatusEffect(new SlowEffect(2, 3)); // Slow movement for 2 turns
                 break;
             case LIGHTNING:
-                
+                ArrayList<Entity> m = this.dun.getMonOnFloor();
+                for (int n = 20; n > 6; n -= 7) {
+                    Entity r = m.get(rand.nextInt(m.size()-1));
+                    r.takeDamage(n+this.currentItem.getDam());
+                    m.remove(r);
+                }
                 break;
+            case ARCANE:
+                if (this.currentItem instanceof Weapon) {
+                    this.currentItem.setArPen(1.5);
+                    target.takeDamage(this.currentItem.getDam());
+                }
         }
     }
 }
