65e6c4000c775a6a509df2321c32ccc56ee8397d
joshuacgunn <181609261+joshuacgunn@users.noreply.github.com>
2025-03-11 16:03:59 -0400

    Added level system

--- Player.java
+++ Player.java
@@ -9,32 +9,43 @@ import java.util.ArrayList;
  */
 public class Player extends Entity {
     private double hp;
-    private double xp;
+    private int xp;
     protected String name;
-    private ArrayList<Item> inventory = new ArrayList<>();
     protected String Class;
-    protected Dungeon dun;
-    private Item currentItem;
+    protected Weapon currentItem;
+    protected int level;
+    protected XPCalculator xpCalculator;
 
-    public Player(String name, String playerClass) {
+    public Player(String name, String Class) {
         super(name);
         this.name = name;
         this.hp = 100;
+        this.level = 1;
+        this.xp = 0;
         this.setArmorRating(1);
-        if (playerClass.equals("1")) {
+        if (Class.equals("1")) {
             this.Class = "Mage";
-        } else if (playerClass.equals("2")) {
+        } else if (Class.equals("2")) {
             this.Class = "Paladin";
-        } else if (playerClass.equals("3")) {
+        } else if (Class.equals("3")) {
             this.Class = "Rogue";
         }
+        this.xpCalculator = new XPCalculator();
     }
-
-    public void enterDungeon(Dungeon d) {
-        this.dun = d;
-        d.pInDun.add(this);
+    public void addXP(int xp) {
+        this.xp += xp;
+        checkLevelUp();
     }
-    // This is AI written. I could not think of a good way to calculate damage with armor.
+    private void checkLevelUp() {
+        int xpNeeded = xpCalculator.calculateXPRequired(level);
+        
+        while (this.xp >= xpNeeded) {
+            this.xp -= xpNeeded; // Carry over excess XP
+            level++;
+            System.out.println("Level up! Now level " + level);
+            xpNeeded = xpCalculator.calculateXPRequired(level);
+        }
+    }
     public void heal(double hpToAdd) {
         if ((hp + hpToAdd) > 100) {
             hp = 100;
@@ -57,5 +68,5 @@ public class Player extends Entity {
         for (Item n : inventory) {
             System.out.println(n.getName());
         }
-    }   
+    }
 }
