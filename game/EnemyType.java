6072aa378e313cf1183fcffc9ceb64763b79a2a0
joshuacgunn <181609261+joshuacgunn@users.noreply.github.com>
2025-03-14 19:21:29 -0400

    Re-organized and added to EnemyType.java

--- game/EnemyType.java
+++ game/EnemyType.java
@@ -1,1 +1,44 @@
-public enum EnemyType {     GOBLIN,     ORC,    SKELETON,   TROLL,      BOSS}
+public enum EnemyType {
+    GOBLIN {
+        @Override
+        public String toString() {
+            return "Goblin";
+        }
+    },
+    ORC {
+        @Override
+        public String toString() {
+            return "Orc";
+        }
+    },
+    TROLL {
+        @Override
+        public String toString() {
+            return "Troll";
+        }
+    },
+    SKELETON {
+        @Override
+        public String toString() {
+            return "Skeleton";
+        }
+    },
+    WIZARD {
+        @Override
+        public String toString() {
+            return "Wizard";
+        }
+    },
+    NECROMANCER {
+        @Override
+        public String toString() {
+            return "Necromancer";
+        }
+    },
+    BOSS {
+        @Override
+        public String toString() {
+            return "Boss";
+        }
+    },
+}
